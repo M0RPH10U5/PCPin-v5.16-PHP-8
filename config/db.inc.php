537a00afe1c199d988aeae2fd691ffeb179fb810
M0RPH10U5 <m0rph10u5@hotmail.com>
2025-08-26 14:03:05 -0500

    Add original files via upload

---
+++ config/db.inc.php
@@ -0,0 +1,25 @@
+<?php
+/* This file contains information needed to connect to database.
+   This file was created automatically during the installation.
+   EDIT IT ONLY IF YOU KNOW WHAT ARE YOU DOING
+ */
+
+/* If you don't know your MySQL access data, then ask your webhosting provider */
+
+
+/* Database server host name or IP address */
+/* Example: 'db.myhost.com' or '127.0.0.1' */
+DEFINE('DBSERVER','localhost');
+
+/* Database username */
+DEFINE('DBLOGIN','root');
+
+/* Database password */
+DEFINE('DBPASSWORD','');
+
+/* Database name */
+DEFINE('DBSEGMENT','pcpin');
+
+/* Prefix for all chat table names */
+DEFINE('PREFIX','pcpin_');
+?>
