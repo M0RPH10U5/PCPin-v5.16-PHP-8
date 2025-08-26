0f9025b4417640d3e026c6d594233ff8c69da41d
M0RPH10U5 <m0rph10u5@hotmail.com>
2025-08-26 14:09:47 -0500

    Add original files via upload

---
+++ include/admin_statistics.inc.php
@@ -0,0 +1,48 @@
+<?PHP
+// Check rights
+IF(!($current_user->level&1)){
+  DIE("");
+}
+
+// Check database tables
+IF($session->testDB()){
+  // One or more tables needs to be optimized.
+  $need_optimization=TRUE;
+  IF($optimize_db){
+    // Optimize database tables
+    $session->optimizeDB();
+    $need_optimization=FALSE;
+  }
+}ELSE{
+  // No database tables optimization needed
+  $need_optimization=FALSE;
+}
+
+// Users
+$user=NEW user();
+$registered_users_count=0;
+$registered_users_online_count=0;
+$guests_count=0;
+$total_users_online_count=0;
+$user->countUsers($session, $registered_users_count, $registered_users_online_count, $guests_count, $total_users_online_count);
+
+// Rooms
+$room=NEW room();
+// Main rooms without password
+$room->listRooms($session,0,"",0);
+$main_rooms_no_pass_count=COUNT($room->roomlist);
+// Main rooms with password
+$room->listRooms($session,0,"",2);
+$main_rooms_pass_count=COUNT($room->roomlist);
+// User rooms without password
+$room->listRooms($session,0,"",1);
+$user_rooms_no_pass_count=COUNT($room->roomlist);
+// Main rooms with password
+$room->listRooms($session,0,"",3);
+$user_rooms_pass_count=COUNT($room->roomlist);
+$total_rooms_count=$main_rooms_no_pass_count+$main_rooms_pass_count+$user_rooms_no_pass_count+$user_rooms_pass_count;
+
+
+// Load teplate
+REQUIRE(TEMPLATEPATH."/admin_statistics.tpl.php");
+?>
